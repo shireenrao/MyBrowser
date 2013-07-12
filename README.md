ec8728eebd53de964ad97f337db2d2e93bedc7c0
shireenrao <shireenrao@gmail.com>
2013-07-12 14:51:45 -0400

    Initial files setup along with ui form

---
+++ README.md
@@ -0,0 +1,5 @@
+MyBrowser
+=========
+
+This is a browser implementation in Python using PyQt. In the process learning
+how to use the eric4 IDE.
---
+++ mybrowser/ui/Ui_mainwindow.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/Users/shireenrao/mydev/python/MyBrowser/mybrowser/ui/mainwindow.ui'
+#
+# Created: Fri Jul 12 14:44:26 2013
+#      by: PyQt4 UI code generator 4.10
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralWidget = QtGui.QWidget(MainWindow)
+        self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralWidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.txtUrl = QtGui.QLineEdit(self.centralWidget)
+        self.txtUrl.setObjectName(_fromUtf8("txtUrl"))
+        self.horizontalLayout.addWidget(self.txtUrl)
+        self.btnNavigate = QtGui.QPushButton(self.centralWidget)
+        self.btnNavigate.setObjectName(_fromUtf8("btnNavigate"))
+        self.horizontalLayout.addWidget(self.btnNavigate)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.webView = QtWebKit.QWebView(self.centralWidget)
+        self.webView.setUrl(QtCore.QUrl(_fromUtf8("about:blank")))
+        self.webView.setObjectName(_fromUtf8("webView"))
+        self.gridLayout.addWidget(self.webView, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralWidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.btnNavigate.setText(_translate("MainWindow", "PushButton", None))
+
+from PyQt4 import QtWebKit
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
