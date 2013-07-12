c019779a2e6abc8e89a9a2e8db535af60afda4b5
shireenrao <shireenrao@gmail.com>
2013-07-12 15:07:00 -0400

    Generate dialog code

---
+++ mybrowser/ui/mainwindow.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module implementing MainWindow.
+"""
+
+from PyQt4.QtGui import QMainWindow
+from PyQt4.QtCore import pyqtSignature
+
+from Ui_mainwindow import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent = None):
+        """
+        Constructor
+        """
+        QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+    
+    @pyqtSignature("")
+    def on_btnNavigate_released(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        raise NotImplementedError
